8c9c30029e83d1ae12e754663d93b6b2bcc8289f
HarmanPreet Singh Saini <harman.henceforth@gmail.com>
2020-04-28 21:52:48 +0530

    patch and delete apis

--- apis/todo.js
+++ apis/todo.js
@@ -73,6 +73,69 @@ router.get('/task/:id',(req,res)=>{
     })
 });
 
+router.patch('/task/:id', (req,res)=>{
+    async.waterfall([
+        function (cb) {
+            var sql = "select * from tasks where id=?";
+            dbqueries.query(sql, [
+                req.params.id
+            ], "row", cb);
+        },
+        function (task, cb) {
+            var sql = "update tasks set title=?, completed=?, `order`=? where id=?";
+            console.log(req.body);
+            if(req.body.completed == 0) {
+                var completed = 0;
+            }
+            else {
+                var completed = req.body.completed ? req.body.completed : task.completed;
+            }
+            
+                dbqueries.query(sql, [
+                    req.body.title ? req.body.title : task.title,
+                    completed,
+                    req.body.order ? req.body.order : task.order,
+                    req.params.id
+                ], "id", cb);
+        },
+        function (id, cb) {
+            var sql = "select * from tasks where id=?";
+            dbqueries.query(sql, [
+                req.params.id
+            ], "row", cb);
+        }
+    ], function (err, result) {
+        console.log(result);
+        var data = responseData(result);
+        console.log("data");
+        
+        console.log(data);
+        
+        apiResponse.response(err, data, res);
+    });
+});
+
+router.delete('/task/:id',(req,res)=>{
+    async.waterfall([
+        function (cb) {
+            var sql = "Delete from tasks where id =?";
+            dbqueries.query(sql, [req.params.id], "affected", cb);
+        }
+    ], function (err, result) {
+        apiResponse.response(err, err ? result : {message:"Task Deleted Successfully"}, res);
+    });
+});
+
+router.delete('/task',(req,res)=>{
+    async.waterfall([
+        function (cb) {
+            var sql = "Delete from tasks";
+            dbqueries.query(sql, [], "affected", cb);
+        }
+    ], function (err, result) {
+        apiResponse.response(err, err ? result : "http://localhost:8000/api/task", res);
+    });
+});
 
 
 function responseData(result) {
